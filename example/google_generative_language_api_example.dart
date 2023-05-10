b48825148698c8b8bc2a9b1e3a56c384e6207583
Saad Ardati <saadodi44@gmail.com>
2023-05-10 21:15:18 +0300

    Initial commit

---
+++ example/google_generative_language_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:google_generative_language_api/google_generative_language_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/google_generative_language_api_test.dart
@@ -0,0 +1,16 @@
+import 'package:google_generative_language_api/google_generative_language_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
